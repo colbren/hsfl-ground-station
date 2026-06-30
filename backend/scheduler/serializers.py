281c9c56fcc6a5aba6690099eb900b469ef1f288
colbren <colfujimoto@gmail.com>
2026-06-29 16:13:50 -1000

    Changing pass status

--- backend/scheduler/serializers.py
+++ backend/scheduler/serializers.py
@@ -1,16 +1,32 @@
+from django.utils import timezone
 from rest_framework import serializers
 from .models import ScheduledPass
 
-
 class ScheduledPassSerializer(serializers.ModelSerializer):
+
     satellite_name = serializers.CharField(
         source="satellite.name",
         read_only=True
     )
+
     ground_station_name = serializers.CharField(
         source="ground_station.name",
         read_only=True
     )
+
+    status = serializers.SerializerMethodField()
+
+    def get_status(self, obj):
+
+        now = timezone.now()
+
+        if now < obj.start_time:
+            return "Pending"
+
+        if obj.start_time <= now <= obj.end_time:
+            return "Running"
+
+        return "Complete"
 
     class Meta:
         model = ScheduledPass
